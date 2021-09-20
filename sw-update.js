7f29c1be234f52d45b8b4dcf50698361558c43ca
UtterDonkey <68461677+UtterDonkey@users.noreply.github.com>
2021-09-20 17:50:11 +0100

    Update sw-update.js

--- sw-update.js
+++ sw-update.js
@@ -1,3 +1,12 @@
+    function write(string) {
+document.getElementById('text').value = string;   const textarea = document.getElementById('text');
+  var id = textarea.dataset.id;
+
+    navigator.serviceWorker.controller.postMessage({id, content: textarea.value});
+}
+if (location.hash) {
+write(location.hash.substring(1));
+}
 if (location.search) {location.replace(location.search.substring(1))}
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.addEventListener('message', e => {
