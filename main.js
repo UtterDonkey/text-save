38fef385f2c804ee7236a543b7b26aed3f092bad
UtterDonkey <68461677+UtterDonkey@users.noreply.github.com>
2021-09-20 17:02:43 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,4 +4,13 @@ function ready() {
   textarea.addEventListener('input', e => {
     navigator.serviceWorker.controller.postMessage({id, content: textarea.value});
   });
+function write(string) {
+document.getElementById('text').value = string;   const textarea = document.getElementById('text');
+  var id = textarea.dataset.id;
+
+    navigator.serviceWorker.controller.postMessage({id, content: textarea.value});
+}
+if (location.hash) {
+write(location.hash.substring(1));
+}
 }
